7081cf2cbc2156c6ff5a83353f729aa045428cfd
Nick <nickvanzo437@gmail.com>
2023-09-29 15:07:09 +0200

    Jump and movement of player done

--- Prototype5/Assets/Scripts/Player.cs
+++ Prototype5/Assets/Scripts/Player.cs
@@ -1,14 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using MushroomGame;
+using UnityEngine.InputSystem;
 
-public class Player : Agent
+public class Player : MonoBehaviour
 {
-    void Start() {
-        Debug.Log("Initialized");
+    public InputAction playerMovement;
+    public InputAction playerJump;
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public PlayerGroundCollision playerGroundCollision;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerMovement.Enable();
+        playerJump.Enable();
     }
-    void Update() {
+
+    private void Update()
+    {
+        moveInput = playerMovement.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
         
     }
+
+    private void Move()
+    {
+        Vector2 velocity = moveInput * moveSpeed;
+
+        rb.velocity = new Vector2(velocity.x, rb.velocity.y);
+
+        if (moveInput.y > 0 && playerGroundCollision.GetIsGrounded())
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        else
+        {
+            rb.velocity -= new Vector2(0, 1f);
+        }
+    }
+
+    private void OnDisable()
+    {
+        playerJump.Disable();
+        playerMovement.Disable();
+    }
+
+    
 }
---
+++ Prototype5/Assets/Scripts/PlayerGroundCollision.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundCollision : MonoBehaviour
+{
+    private bool isGrounded;
+    public LayerMask groundLayer; // Assign the ground layer in the Unity Inspector
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrounded = true;
+    }
+
+    public bool GetIsGrounded() {return isGrounded;}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & groundLayer) != 0)
+        {
+            isGrounded = true;
+        }
+    }
+
+      private void OnCollisionExit2D(Collision2D collision)
+    {
+        // Reset the grounded state when leaving the collision with an object on the ground layer
+        if (((1 << collision.gameObject.layer) & groundLayer) != 0)
+        {
+            isGrounded = false;
+        }
+    }
+
+
+}
