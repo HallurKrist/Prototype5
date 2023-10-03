cf8925f99a66407636e75c5726a90832632498e6
Steffen Jørgensen <stejo@itu.dk>
2023-10-03 02:05:02 +0200

    Flashing text added

--- Prototype5/Assets/Scripts/PressAnyKey.cs
+++ Prototype5/Assets/Scripts/PressAnyKey.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class PressAnyKey : MonoBehaviour
 {
+    // Setting fading variables
+    bool fading = true;
+    public float MinFade = 0.5f;
+    public float FadeSpeed = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +24,26 @@ public class PressAnyKey : MonoBehaviour
         if(Input.anyKey) {
             // For now, "test" is the scene to load
             SceneManager.LoadScene("test");
+        }
+
+        // Fetch current color property of the Sprite Renderer component
+        Color clr = GetComponent<SpriteRenderer>().color;
+        if(fading) {
+            clr.a -= FadeSpeed * Time.deltaTime;
+            if(clr.a <= MinFade) {
+                fading = false;
+            } else {
+                // Apply the update to alpha
+                GetComponent<SpriteRenderer>().color = clr;
+            }
+        } else {
+            clr.a += FadeSpeed * Time.deltaTime;
+            if(clr.a >= 1f) {
+                fading = true;
+            } else {
+                // Apply the update to alpha
+                GetComponent<SpriteRenderer>().color = clr;
+            }
         }
     }
 }
